ac5cda3b7cf298081ed769a82c060fa24eec11ab
ManuelRiascos <manuel.riascos2008@gmail.com>
2025-11-26 17:46:14 -0500

    Se elimino la clase inventario porque se la agregara en la guild, y se modifico la guild

--- src/Guild.cpp
+++ src/Guild.cpp
@@ -1,21 +1,41 @@
+#include "guild.h"
 #include "Guild.h"
-#include "personaje.h"
+#include "Guerrero.h"
+#include "Mago.h"
+#include "Sanador.h"
+#include "Berserker.h"
+#include "Lupasos.h"
+#include "PocionVida.h"
+#include "AmuletoFuria.h"
+#include "EscudoBendito.h"
+#include "DanioElectrico.h"
+#include "Tornado.h"
 #include <iostream>
+#include <fstream>
+#include "DagaSombria.h"
+
+Guild::Guild(string nombre) : nombreGuild(nombre), contadorId(1) {}
 
 Guild::~Guild() {
-    for (auto p : heroes) delete p;
+    for (auto& par : heroes) {
+        delete par.second;
+    }
+    for (ObjetoMagico* obj : inventario) {
+        delete obj;
+    }
 }
 
-void Guild::agregar(Personaje *p) { heroes.push_back(p); }
+void Guild::inicializarHeroes() {
+    agregarHeroe(new Guerrero("Arthos", 5, 120, 25, 15));
+    agregarHeroe(new Mago("Lyra", 5, 80, 30, 8));
+    agregarHeroe(new Sanador("Elara", 5, 90, 15, 10));
+    agregarHeroe(new Berserker("Ragnar", 5, 110, 28, 12));
+    agregarHeroe(new Lupasos("Fenrir", 5, 100, 22, 13));
 
-void Guild::listar() const {
-    for (auto p : heroes)
-        std::cout << p->getId() << " - " << p->getNombre() << " vida=" << p->getVida() << "\n";
+    agregarObjeto(new PocionVida(3));
+    agregarObjeto(new AmuletoFuria(2));
+    agregarObjeto(new EscudoBendito(2));
+    agregarObjeto(new DanioElectrico(2));
+    agregarObjeto(new Tornado(2));
+    agregarObjeto(new DagaSombria(3));
 }
-
-Personaje* Guild::obtenerPorId(int id) const {
-    for (auto p : heroes) if (p->getId() == id) return p;
-    return nullptr;
-}
-
-std::vector<Personaje*>& Guild::getHeroes() { return heroes; }
