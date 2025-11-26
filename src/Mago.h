0f45f8286b2dc23b4777fac4b06614173bd4c311
ManuelRiascos <manuel.riascos2008@gmail.com>
2025-11-26 11:53:08 -0500

    Se elimino la clase heroe ya que miramos con mi compañero que generaba conflictos al hacer las personajes nuevos, tambien se le agrego al personaje sanador sus opcines y el ataque

--- src/Mago.h
+++ src/Mago.h
@@ -1,8 +1,12 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MANUELR_LUPIM_MAGO_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MANUELR_LUPIM_MAGO_H
 
+#include "Personaje.h"
 
-class Mago {
+class Mago : public Personaje {
+public:
+    Mago(string n, int nv, int v, int a, int d);
+    void realizarAccion(Personaje* objetivo) override;
 };
 
 
