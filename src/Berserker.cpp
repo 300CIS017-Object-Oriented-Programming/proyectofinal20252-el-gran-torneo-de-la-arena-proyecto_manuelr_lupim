75ccf06544b9d0b7d421115b5aab9142dd85e5d4
ManuelRiascos <manuel.riascos2008@gmail.com>
2025-11-26 12:01:49 -0500

    Se agrego el personaje berserker con algunas funciones

--- src/Berserker.cpp
+++ src/Berserker.cpp
@@ -1,1 +1,18 @@
-#include "Berserker.h".
+#include "Berserker.h"
+#include <iostream>
+#include <cstdlib>
+using std::endl;
+using std::cout;
+
+Berserker::Berserker(string n, int nv, int v, int a, int d)
+    : Personaje(n, nv, v, a, d, "Berserker"), modoFuria(false), turnosEnFuria(0) {}
+
+void Berserker::activarFuria() {
+    if (!modoFuria) {
+        modoFuria = true;
+        turnosEnFuria = 3;
+        ataque = ataque * 1.5; // Aumenta 50% el ataque
+        defensa = defensa / 2;  // Reduce 50% la defensa
+        cout << "¡" << nombre << " entra en MODO FURIA! Ataque aumentado pero defensa reducida." << endl;
+    }
+}
