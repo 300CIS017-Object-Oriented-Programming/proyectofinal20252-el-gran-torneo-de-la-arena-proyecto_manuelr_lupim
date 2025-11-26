487782e629e2065c8be1c024c3daa2721193ea19
ManuelRiascos <manuel.riascos2008@gmail.com>
2025-11-26 17:39:08 -0500

    Se arregla errores en arena.cpp , y se terminan los metodos del Berserker(personaje)

--- src/Berserker.cpp
+++ src/Berserker.cpp
@@ -25,3 +25,42 @@ void Berserker::desactivarFuria() {
         cout << nombre << " sale del modo furia y recupera su defensa." << endl;
     }
 }
+void Berserker::realizarAccion(Personaje* objetivo) {
+    if (!objetivo || !objetivo->estaVivo()) {
+        cout << nombre << " no tiene objetivo valido." << endl;
+        return;
+    }
+
+    // Si la vida esta baja (menos del 40%), entra en furia
+    if (vida < (vidaMaxima * 0.4) && !modoFuria) {
+        activarFuria();
+    }
+
+    // Realizar ataque
+    int danio = ataque;
+    int variacion = rand() % 11 - 5; // -5 a +5
+    danio += variacion;
+
+    if (danio < 0) danio = 0;
+
+    if (modoFuria) {
+        cout << nombre << " ataca con FURIA SALVAJE a " << objetivo->getNombre()
+             << "! Dano: " << danio << endl;
+    } else {
+        cout << nombre << " ataca ferozmente a " << objetivo->getNombre()
+             << ". Dano: " << danio << endl;
+    }
+
+    objetivo->recibirDanio(danio);
+
+    cout << objetivo->getNombre() << " tiene "
+         << objetivo->getVida() << " puntos de vida." << endl;
+
+    // Gestionar turnos de furia
+    if (modoFuria) {
+        turnosEnFuria--;
+        if (turnosEnFuria <= 0) {
+            desactivarFuria();
+        }
+    }
+}
