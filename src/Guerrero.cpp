fb9cc49909cbb67a2b3c15feb96a0d558444fc7c
ManuelRiascos <manuel.riascos2008@gmail.com>
2025-11-26 01:14:48 -0500

    Se implementa la clase Guerrero

--- src/Guerrero.cpp
+++ src/Guerrero.cpp
@@ -1,1 +1,19 @@
+#include "guerrero.h"
 #include "Guerrero.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+Guerrero::Guerrero(string n, int nv, int v, int a, int d)
+    : Personaje(n, nv, v, a, d, "Guerrero"), probabilidadCritico(0.2f) {}
+
+void Guerrero::realizarAccion(Personaje* objetivo) {
+    if (!objetivo || !objetivo->estaVivo()) {
+        cout << nombre << " no tiene objetivo valido." << endl;
+        return;
+    }
+
--- src/Guerrero.h
+++ src/Guerrero.h
@@ -1,8 +1,15 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MANUELR_LUPIM_GUERRERO_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MANUELR_LUPIM_GUERRERO_H
 
+#include "Personaje.h"
 
-class Guerrero {
+class Guerrero : public Personaje {
+private:
+    float probabilidadCritico;
+
+public:
+    Guerrero(string n, int nv, int v, int a, int d);
+    void realizarAccion(Personaje* objetivo) override;
 };
 
 
